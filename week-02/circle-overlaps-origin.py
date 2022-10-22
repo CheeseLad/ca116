82f2fb07fcb170f1c06f61790f6816a14290cd6d
Cheese McCheese <52334071+CheeseLad@users.noreply.github.com>
2022-10-22 17:58:19 +0100

    Add files via upload

--- week-02/circle-overlaps-origin.py
+++ week-02/circle-overlaps-origin.py
@@ -4,9 +4,22 @@ x = int(input())
 y = int(input())
 r = int(input())
 
+<<<<<<< Updated upstream
 #distance = (((x2 - x1) ** 2 + (y2 - y1) ** 2) ** .5)
+=======
+#distance = (((0 - x) ** 2 + (0 - y) ** 2) ** .5)
+>>>>>>> Stashed changes
 
 #print(x * r != 0)
 #print(x // y // r == 0)
 
+<<<<<<< Updated upstream
 print(x + y >= r)
+=======
+#print(r >= x + y)
+
+#print(x ** 2 + y ** 2 == r ** 2)
+#print(distance > x ** 2 + y ** 2)
+
+print(x * x + y * y < r * r)
+>>>>>>> Stashed changes
--- week-02/picker.py
+++ week-02/picker.py
@@ -4,5 +4,12 @@ a = int(input())
 b = int(input())
 c = int(input())
 
+<<<<<<< Updated upstream
 print(c % 2 == 0)
 print(c * (c % 2))
+=======
+t = c % 2 == 0
+y = c % 2 > 0
+
+print(int(t) * a or int(y) * b)
+>>>>>>> Stashed changes
